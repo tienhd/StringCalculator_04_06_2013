ad31f1c27f74727f9ca357f505a62e196a4170e7
Tien <Hoang>
2013-06-04 14:15:27 +0700

    go to test 8 : RED

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,108 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: sqv-nbt
+ * Date: 6/4/13
+ * Time: 1:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringCalculator {
+
+    public int result = 0;
+    public int Add (String inputString) {
+        if (inputString.isEmpty()) {
+            return result;
+        }
+        else {
+            String regex = "[,;\\n//*#]";
+            String negative = "-([\\d])(.*)";
+            String define = "(//)(\\[)(.*)(\\])(\\n)(.*)";
+
+            String negativeNumbers = "";
+            boolean ok = false;
+
+            if (inputString.matches(define)) {
+                regex = getDefineDelimiter(inputString);
+                String tempString = regex;
+                String[] getString = inputString.split("\\n");
+                inputString = getString[1];
+                String temp = "";
+                for (int j = 0 ; j< tempString.length(); j++) {
+                    char t = tempString.charAt(j);
+                    if ((t == '*') || (t == '?') || (t == '+') || (t =='[') || (t==']') || ( t== '(') || ( t==')')) {
+                        temp += "\\" + t;
+                    }
+                    else {
+                        temp += t;
+                    }
+                }
+                regex = temp;
+
+                System.out.println(regex);
+            }
+            String[] inputNumbers = inputString.split(regex);
+            for (String si : inputNumbers) {
+                if (si.matches(negative)) {
+                    ok = true;
+                    negativeNumbers += si + " ";
+                    continue;
+                }
+                if (ok) {
+                    //System.out.println(negativeNumbers);
+                    throw new IllegalArgumentException("Negative is not allowed. " + negativeNumbers );
+                }
+                if ((!si.isEmpty()) && (!si.equals("[")) && (!si.equals("]"))) {
+                    int number = Integer.parseInt(si);
+                    if (number <= 1000) {
+                        result += number;
+                    }
+
+                }
+            }
+            return result;
+        }
+    }
+
+    public String getDefineDelimiter(String inputString) {
+        String result = "";
+        String getPattern = "(//)(.*)(\\n)(.*)";
+        if (inputString.matches(getPattern)) {
+            //System.out.println("MATCH");
+            String[] preDefine = inputString.split("\\n");
+            String defineDelimiter = preDefine[0].substring(3,preDefine[0].length()-1);
+            System.out.println(defineDelimiter);
+            result = defineDelimiter;
+        }
+        return result;
+    }
+
+    public String getMultiDefineDelimiter(String inputString) {
+        String result = "";
+        result += "[";
+        String getPattern = "(//)(.*)(\\n)(.*)";
+        if (inputString.matches(getPattern)) {
+//            System.out.println("MATCH MULTI");
+            String[] temp = inputString.split("\\n");
+            String[] preDefine = temp[0].split("//");
+            String tempDelimiter = preDefine[1];
+            System.out.println(tempDelimiter);
+            String pattern = "(\\[)(.*)(\\])(.*)";
+            if (tempDelimiter.matches(pattern)) {
+//                System.out.println("M");
+                tempDelimiter = tempDelimiter.replaceAll("\\["," ");
+                tempDelimiter = tempDelimiter.replaceAll("\\]"," ");
+                String[] delimiter = tempDelimiter.split(" ");
+                for (String di : delimiter) {
+                    System.out.println(di);
+                }
+                for (String di: delimiter) {
+                    if (!di.isEmpty()) {
+                        result += "(" + di + ")";
+                    }
+                }
+
+            }
+            result += "]";
+        }
+        return result;
+    }
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,90 @@
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sqv-nbt
+ * Date: 6/4/13
+ * Time: 1:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringCalculatorTest {
+    @Test
+    public void TestWithNoInputNumber() {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(0, stringCalculator.Add(""));
+    }
+
+    @Test
+    public void TestWithTwoInputNumber () {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(3,stringCalculator.Add("1,2"));
+    }
+
+    @Test
+    public void TestWithMoreInputNumber () {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(6, stringCalculator.Add("1,3,2"));
+    }
+
+    @Test
+    public void TestEndlineInInputString () {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(6,stringCalculator.Add("1,2\n3"));
+    }
+
+    @Test
+    public void TestWithDifferentDelimiter () {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(6,stringCalculator.Add("//;\n1;2;3"));
+    }
+
+    @Test
+    public void TestWithDifferentDelimiterAdvance() {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(6,stringCalculator.Add("//#\n1#2#3"));
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void TestNegativeExceptionInInputString() {
+        StringCalculator stringCalculator = new StringCalculator();
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Negative is not allowed. -1");
+        stringCalculator.Add("//;\n,-1;2,3");
+    }
+
+    @Test
+    public void TestWithInputNumberOverThanOneThousand () {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(5,stringCalculator.Add("3,2;1001"));
+    }
+
+    @Test
+    public void TestWithDefineStringDelimiter() {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(6, stringCalculator.Add("//[***]\n1***2***3"));
+    }
+
+    @Test
+    public void TestGetDefineDelimiterModule() {
+        StringCalculator scModule = new StringCalculator();
+        Assert.assertEquals("***",scModule.getDefineDelimiter("//[***]\n1***2***3)"));
+    }
+
+    @Test
+    public void TestGetMultiDefineDelimiterModule() {
+        StringCalculator scModule = new StringCalculator();
+        Assert.assertEquals("[(***)(;)]",scModule.getMultiDefineDelimiter("//[***][;]\n1***2;3"));
+    }
+
+    @Test
+    public void TestWithMultiDefineDelimiter(){
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(6, stringCalculator.Add("//[***][;]\n1***2;3"));
+    }
+}
